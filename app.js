159fa3fe65586908ef4cd76c4e1370f8b93aec89
Jeffrey Goldbeck <jeffg5001@gmail.com>
2018-01-15 18:44:59 -0500

    set up listeners and logging functions

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+
+app.listen(3000, (req,res) => {
+    console.log('server listening');
+});
+
+app.use('/*',(req,res,next)=>{
+    console.log(req.method + ' ' + req.url + ' ' + res.statusCode);
+    next();
+})
+
+app.use('/special*',(req,res,next)=>{
+    res.send('You\'ve reached the special area!')
+    next();
+})
+
+app.get('/', (req,res) => {
+    res.send('Welcome to our website!');
+})
+
