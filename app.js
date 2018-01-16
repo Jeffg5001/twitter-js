c6f462d6b32283ef374856ea27f315a3f0c9e6ad
Jeffrey Goldbeck <jeffg5001@gmail.com>
2018-01-16 10:50:53 -0500

    Installed nunjucks and successfully rendered and set engine

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
@@ -16,22 +17,28 @@ app.use('/special*',(req,res,next)=>{
     res.send('You\'ve reached the special area!')
     next();
 })
-
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views',{noCache:true})
+const people = [{name: 'Phil'}, {name: 'Jeff'}, {name: 'Son'}];
+var different = true
 app.get('/', (req,res) => {
-    res.send('Welcome to our website!');
+    res.render( 'index', {title: 'Hall of Fame', people: people, test:different} );
 })
 
-const nunjucks = require('nunjucks');
 
-var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function (err, output) {
-    console.log(output);
-});
+//res.render( 'index', {title: 'Hall of Fame', people: people} );
+
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
+// nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//     if(err){throw err};
+//     console.log(output);
+// });
